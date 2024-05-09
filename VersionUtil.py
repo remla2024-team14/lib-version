70055aac3eceae869f67e3ecd690a6c82aef3988
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-09 19:05:03 +0200

    Added api endpoint
    Added api endpoint with Flask

--- VersionUtil.py
+++ VersionUtil.py
@@ -1,8 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
 class VersionUtil:
     @staticmethod
+    @app.route('/lib-version', methods=['GET'])
     def get_version():
         try:
             with open("VERSION.txt", "r") as file:
-                return "App v" + file.read().strip()
+                return "lib-version v" + file.read().strip()
         except FileNotFoundError:
             return "Version information not found"
+
+if __name__ == '__main__':
+    app.run(debug=False)
