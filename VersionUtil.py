dc410b21582fa3d0c943ca0f037356e0cea7afb6
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-09 22:32:17 +0200

    Quickfix CORS
    Enable CORS because of applications on diff ports.

--- VersionUtil.py
+++ VersionUtil.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 class VersionUtil:
     @staticmethod
     @app.route('/lib-version', methods=['GET'])
     def get_version():
         try:
             with open("VERSION.txt", "r") as file:
-                return "lib-version v" + file.read().strip()
+                return jsonify("lib-version: " + file.read().strip())
         except FileNotFoundError:
             return "Version information not found"
 
