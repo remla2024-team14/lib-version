f4c77e32d8d2141ddbd348031c892a6911a9feb6
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-10 21:20:21 +0200

    add release to pypi
    Should release to pypi, almost the same workflow as lib-mil

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='team-14-lib-version',
+    version='0.1.0',
+    packages=find_packages(),
+    description='version aware library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Team 14',
+    author_email='your.email@example.com',
+    url='https://github.com/remla2024-team14/lib-version',
+    install_requires=[
+        'flask',  'flask-cors'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    extras_require={'dev': ['twine>=4.0.2']},
+    python_requires='>=3.8',
+)
