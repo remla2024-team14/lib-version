0e51814a90db47644c37be348c39b5e5b6586e7c
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-11 11:53:35 +0200

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='team-14-lib-version',
-    use_scm_version=True,
+    use_scm_version={"root": ".", "relative_to": __file__},
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     description='version aware library',
@@ -15,14 +15,10 @@ setup(
         'flask',  'flask-cors'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    extras_require={'dev': ['twine>=4.0.2']},
+    python_requires='>=3.8',
 )
