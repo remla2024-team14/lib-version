56245940471e70478dd8d4dce6cf25844901118e
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-11 13:26:36 +0200

    test

--- lib-version/VersionUtil.py
+++ lib-version/VersionUtil.py
@@ -1,12 +1,19 @@
-import importlib.metadata
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
 
-
+app = Flask(__name__)
+CORS(app)
 class VersionUtil:
     @staticmethod
+    @app.route('/lib-version', methods=['GET'])
     def get_version(package_name="team_14_lib_version"):
         try:
             dist = importlib.metadata.distribution(package_name)
-            print(dist.version)
-            return dist.version
+            return jsonify(dist.version)
         except importlib.metadata.PackageNotFoundError:
             return f"No package named '{package_name}' found."
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
