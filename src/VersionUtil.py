02a5a2387c1446d8e98efd43a60ba298b8d44d7f
Justinluu2311 <J.Luu@student.tudelft.nl>
2024-05-11 13:07:04 +0200

    test
    test

--- src/VersionUtil.py
+++ src/VersionUtil.py
@@ -1,30 +1,12 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import requests
+import importlib.metadata
 
-app = Flask(__name__)
-CORS(app)
+
 class VersionUtil:
     @staticmethod
-    @app.route('/lib-version', methods=['GET'])
-    def get_version(url="https://api.github.com/repos/remla2024-team14/lib-version/tags"):
+    def get_version(package_name="team_14_lib_version"):
         try:
-            response = requests.get(url)
-            response.raise_for_status()  # Raise exception for HTTP errors
-            tags = response.json()
-
-            latest_version = max(tag["name"] for tag in tags)
-            with open('../version.txt', 'w') as file:
-                file.write(latest_version)
-            return jsonify("lib-version: " + latest_version)
-        except requests.exceptions.RequestException:  # Handles all requests-related exceptions
-            try:
-                with open("../version.txt", "r") as file:
-                    last_saved_version = file.read().strip()
-                    return jsonify("last saved lib-version: " + last_saved_version)
-            except FileNotFoundError:
-                return jsonify("Error: No saved version found.")
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+            dist = importlib.metadata.distribution(package_name)
+            print(dist.version)
+            return dist.version
+        except importlib.metadata.PackageNotFoundError:
+            return f"No package named '{package_name}' found."
